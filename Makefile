cca6d5be7df131c5eb4135f9da9523f1eb556010
Ghazaleh <ghazalehansari1373@gamil.com>
2025-02-10 12:43:39 +0100

    multiple pipes is handled in the bonus

--- Makefile
+++ Makefile
@@ -1,14 +1,18 @@
 NAME        = pipex
 
 SRCS_FILES  = main.c pipex.c pipex_utils.c
+BONUS_FILES = main_bonus.c pipex_bonus.c pipex_utils_bonus.c
 
 SRCS_MAN    = $(SRCS_FILES)
+SRCS_BONUS	= $(BONUS_FILES)
 OBJS_MAN    = $(SRCS_MAN:.c=.o)
+OBJS_BONUS	= $(SRCS_BONUS:.c=.o)
 
 LIB_DIR     = Libft
 LIB_NAME    = libft.a
 LIBFT       = $(LIB_DIR)/$(LIB_NAME)
 HEADER      = -I./pipex.h
+HEADER_B	=-I./pipex_bonus.h
 
 CC          = cc
 CFLAGS      = -Wall -Wextra -Werror
@@ -19,11 +23,20 @@ all: $(NAME)
 $(NAME): $(OBJS_MAN) pipex.h
 	@echo "\033[38;2;0;0;255m╰⊱♥⊱╮ღ꧁ PIPEX by Ghazaleh ꧂ღ╭⊱♥≺"
 	@echo "\033[38;2;128;0;128m\n(✿◠‿◠)\n"
-	@echo "\033[38;2;255;105;180m+++++Compiling the libft+++++"
+	@echo "\033[38;2;255;105;180m+++++Compiling the libft+++++\n"
 	@make -C $(LIB_DIR)
 	@$(CC) $(CFLAGS) $(HEADER) -I. $(OBJS_MAN) -L$(LIB_DIR) -lft -o $(NAME)
-	@echo "\033[38;2;0;255;0mThe coolest PIPEX is ready! ᕕ( ᐛ )ᕗ"
+	@#clear
+	@echo "\033[38;2;0;255;0mThe coolest PIPEX is ready! ᕕ( ᐛ )ᕗ\n"
 
+bonus: $(OBJS_BONUS) pipex_bonus.h
+	@echo "\033[38;2;0;0;255m╰⊱♥⊱╮ღ꧁ PIPEX BONUS by Ghazaleh ꧂ღ╭⊱♥≺"
+	@echo "\033[38;2;128;0;128m\n(✿◠‿◠)\n"
+	@echo "\033[38;2;255;105;180m+++++Compiling the libft+++++\n"
+	@make -C $(LIB_DIR)
+	@$(CC) $(CFLAGS) $(HEADER_B) -I. $(OBJS_BONUS) -L$(LIB_DIR)  -lft -o $(NAME)
+	@#clear
+	@echo "\033[38;2;0;255;0mYES! It's bonus time! ─=≡Σʕっ•ᴥ•ʔっ\n"
 %.o: %.c
 	@$(CC) $(CFLAGS) $(HEADER) -c $< -o $@
 
--- pipex_bonus.c
+++ pipex_bonus.c
@@ -12,59 +12,61 @@
 
 #include "pipex_bonus.h"
 
-void	write_to_limiter(int *fd, char *limiter)
+void	exec_cmd(char *cmd, char *envp[])
 {
-	char	*line;
+	int	fd[2];
+	int	pid;
 
-	close(fd[0]);
-	ft_putstr_fd("pipe heredoc> ", 1);
-	line = get_next_line(0);
-	while (line)
-	{
-		if (ft_strcmp(limiter, line) == 0)
-		{
-			free(line);
-			close(fd[1]);
-			exit(EXIT_SUCCESS);
-		}
-		ft_putstr_fd("pipe in here_doc> ", 1);
-		if (write(fd[1], line, ft_strlen(line)) == -1)
-			perror("ERROR(write input in here_doc)");
-		if (write(fd[1], "\n", 1) == -1)
-			perror("ERROR(write new line in here_doc)");
-		free(line);
-		line = get_next_line(0);
-	}
-	free(line);
-	close(fd[1]);
-	ft_perror("ERROR - 13");
-}
-
-void	here_doc(int argc, char **argv)
-{
-	int		fd[2];
-	int		pid;
-
-	if (argc < 6)
-		ft_error("Error: Wrong count of arguments -- argc < 6");
 	if (pipe(fd) == -1)
-		perror("ERROR(pipe in here_doc)");
+		ft_perror("ERROR - 3");
 	pid = fork();
 	if (pid == -1)
-		ft_perror("ERROR(fork in here_doc)");
+		ft_perror("ERROR - 4");
 	if (pid == 0)
-		write_to_limiter(fd, argv[2]);
-	else
 	{
-		close(fd[1]);
-		if (dup2(fd[0], STDIN_FILENO) == -1)
-			ft_perror("ERROR - 14");
 		close(fd[0]);
-		waitpid(pid, NULL, 0);
+		if (dup2(fd[1], STDOUT_FILENO) == -1)
+			ft_perror("ERROR - 5");
+		close(fd[1]);
+		ft_exec(cmd, envp);
 	}
+	close(fd[1]);
+	if (dup2(fd[0], STDIN_FILENO) == -1)
+		ft_perror("ERROR - 6");
+	close(fd[0]);
+	waitpid(pid, NULL, 0);
+
 }
 
-void	pipex(int argc, char **argv, char **envp)
+void	ft_exec(char *cmd, char **envp)
+{
+	int		i;
+	char	**commands;
+	char	**paths;
+	char	*the_way;
+
+	commands = ft_split(cmd, ' ');
+	paths = parse_path(envp);
+	if (paths == NULL)
+		ft_error("Error: no PATH");
+	i = 0;
+	while (paths[i])
+	{
+		the_way = ft_makepath(paths[i], commands[0]);
+		if (access(the_way, F_OK) == 0)
+		{
+			if (execve(the_way, commands, NULL) == -1)
+				ft_perror("ERROR(executing cmd)");
+		}
+		free(the_way);
+		i++;
+	}
+	free_array(commands);
+	free_array(paths);
+	ft_perror("ERROR(no execution)");
+}
+
+void	pipex_bonus(int argc, char **argv, char **envp)
 {
 	int	num;
 	int	fd_in;
@@ -72,21 +74,20 @@ void	pipex(int argc, char **argv, char **envp)
 
 	if (ft_strcmp(argv[1], "here_doc") == 0)
 	{
-		num = 3;
-		here_doc(argc, argv);
-		fd_out = opener(argv[argc - 1], 'h');
+		num = 0;
+
 	}
 	else
 	{
 		num = 2;
-		fd_in = opener(argv[1], 'I');
-		fd_out = opener(argv[argc - 1], 'O');
+		fd_in = file_opener(argv[1], 'I');
+		fd_out = file_opener(argv[argc - 1], 'O');
 		if (dup2(fd_in, STDIN_FILENO) == -1)
 			ft_perror("ERROR - 1");
 	}
 	while (num < argc - 2)
-		command_executing(argv[num++], envp);
+		exec_cmd(argv[num++], envp);
 	if (dup2(fd_out, STDOUT_FILENO) == -1)
 		ft_perror("ERROR - 2");
-	executer(argv[argc - 2], envp);
+	ft_exec(argv[argc - 2], envp);
 }
